cbad92a3b51c70ac8e7b21af59067409e831a3e9
Ahrirg <ldargevicius20@gmail.com>
2026-02-07 20:40:30 +0200

    Types: added types for global import

---
+++ src/Types/DiscountTypes.ts
@@ -0,0 +1,26 @@
+export interface Magazine {
+  MagazineID?: number;
+  EndTime: string;
+  AddedTime: string;
+  URL: string;
+}
+
+export interface Page {
+  PageId?: number;
+  EndTime: string;
+  AddedTime: string;
+  ImageUUID: string;
+  Parsed: number;
+  MagazineId: number;
+}
+
+export interface ProductAkcija {
+  ProductName: string;
+  ShopName: string;
+  DiscountSizeProc: number;
+  CostBeforeDiscount: number;
+  CostAfterDiscount: number;
+  EndTime: string;
+  AddedTime: string;
+  PageId: number;
+}
