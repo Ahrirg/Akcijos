537b2d3772565a65b2731c00365bad08379e2cfd
Ahrirg <ldargevicius20@gmail.com>
2026-02-11 17:43:16 +0200

    All: updated data folder location

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config';
+import path from 'path';
+
 
 export const CONFIG = {
-  DB_PATH: process.env.DB_PATH ?? './default.db',
+  DB_PATH: process.env.DB_PATH ?? path.join(__dirname, '..', '..' ,'data'),
   PORT: Number(process.env.PORT) || 3000,
   OLLAMA_SERVER: process.env.OLLAMA_SERVER || "http://localhost:11434",
   GOOGLE_API: process.env.GOOGLE_API,
